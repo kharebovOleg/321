17109349ab0c2cdbed1c4c81ec9e8121e6ab567e
oleg_kharebov <xarebov1993@yandex.ru>
2022-05-08 20:56:18 +0300

    first try

---
+++ Check.java
@@ -0,0 +1,12 @@
+public class Check {
+    public static void checkUserName(String first, String second) {
+        boolean check = first.equalsIgnoreCase(second);
+        if (check) {
+            System.out.println("Выберите другое имя пользователя");
+        } else {
+            System.out.println("Отличное имя!");
+            System.out.println("Ваше имя имеет длину " + first.length() + " символов");
+            System.out.println("А без пробелов ваше имя занимает " + first.replace(" ", "").length() + " символов");
+        }
+    }
+}
---
+++ Main.java
@@ -0,0 +1,5 @@
+public class Main {
+    public static void main(String[] args) {
+        Check.checkUserName("иван петров", "вован");
+    }
+}
